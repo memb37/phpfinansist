db66dc54119f137b41ab1027c22a525ab5fcfbed
memb37 <memb37@gmail.com>
2014-02-15 02:03:23 +0400

    Добавлена страница профиля пользователя #4

--- header.php
+++ header.php
@@ -3,3 +3,10 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
+<?php
+require_once "check.php";
+if ($auth)
+{
+	echo "<a href = \"profile.php\">профиль</a>";
+}
+?>
