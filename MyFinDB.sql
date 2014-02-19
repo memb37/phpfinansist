95ac1486e0a354a3de2e9e21441ec386fcf7b79f
memb37 <memb37@gmail.com>
2014-02-20 03:03:00 +0400

    rename column #17

--- MyFinDB.sql
+++ MyFinDB.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Фев 20 2014 г., 02:33
+-- Время создания: Фев 20 2014 г., 03:02
 -- Версия сервера: 5.5.34-0ubuntu0.12.10.1
 -- Версия PHP: 5.4.6-1ubuntu1.5
 
@@ -23,6 +23,67 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `category_type_id` int(11) NOT NULL,
+  `category_name` varchar(30) NOT NULL,
+  PRIMARY KEY (`category_id`),
+  KEY `operation_type_id` (`category_type_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=99 ;
+
+--
+-- Дамп данных таблицы `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `user_id`, `category_type_id`, `category_name`) VALUES
+(85, 777, 1, 'без категории'),
+(86, 777, 2, 'без категории');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `category_type`
+--
+
+CREATE TABLE IF NOT EXISTS `category_type` (
+  `category_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_type_name` varchar(30) NOT NULL,
+  PRIMARY KEY (`category_type_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Дамп данных таблицы `category_type`
+--
+
+INSERT INTO `category_type` (`category_type_id`, `category_type_name`) VALUES
+(1, 'расход'),
+(2, 'доход');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `operations`
+--
+
+CREATE TABLE IF NOT EXISTS `operations` (
+  `operation_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `summ` int(20) NOT NULL,
+  `date` date NOT NULL,
+  `comment` text NOT NULL,
+  PRIMARY KEY (`operation_id`),
+  KEY `category_id` (`category_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=172 ;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `users`
 --
 
@@ -34,6 +95,17 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(32) NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=52 ;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `operations`
+--
+ALTER TABLE `operations`
+  ADD CONSTRAINT `operations_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `operations_ibfk_4` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
