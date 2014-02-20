e184562f826763212087167592444847020483f5
memb37 <memb37@gmail.com>
2014-02-20 04:36:34 +0400

    included twitter bootstrap #11

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -2,6 +2,10 @@
 <html lang="ru">
 <head>
     <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">   
+    <script src="//code.jquery.com/jquery-2.0.3.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
     <title>Главная</title>
 </head>
 <body>
