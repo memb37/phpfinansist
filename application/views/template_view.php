7077481210cbb33e5e3ba94cb0af0ff2e09253dd
notapresent <notapresent@gmail.com>
2014-02-22 15:56:11 +0400

    Использовать локальную копию bootstrap

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -4,9 +4,9 @@
     <base href="<?=BASE_URL?>">
     <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">   
-    <script src="//code.jquery.com/jquery-2.0.3.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">   
+    <script src="js/jquery-2.0.3.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="css/style.css"> 
     <title>Главная</title>
 </head>
