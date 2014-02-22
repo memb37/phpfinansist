c4109aab1bc7b89873c37afd5213b3cd22dd6abf
notapresent <notapresent@gmail.com>
2014-02-22 14:47:52 +0400

    Исправить 404

--- application/core/route.php
+++ application/core/route.php
@@ -60,11 +60,9 @@ class Route
     
     }
     
-    function ErrorPage404()
+    public static function ErrorPage404()
     {
-        $host = 'http://'.$_SERVER['HTTP_HOST'].'/';
-        header('HTTP/1.1 404 Not Found');
-        header("Status: 404 Not Found");
-        header('Location:'.$host.'404');
+        header('HTTP/1.1 404 Not Found', true, 404);
+        exit('<h1>404 Not found</h1>');
     }
 }
