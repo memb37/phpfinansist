987486e4d788814fa4ad896400173a7d58dd5412
memb37 <memb37@gmail.com>
2014-02-25 01:11:53 +0400

    убрать записи "(без категории)", убрать таблицу category_type

--- MyFinDB.sql
+++ MyFinDB.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.35, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.35, for debian-linux-gnu (i686)
 --
--- Host: localhost    Database: phpfinansist_dev
+-- Host: localhost    Database: MyFinDB
 -- ------------------------------------------------------
--- Server version	5.5.35-0ubuntu0.12.04.2
+-- Server version	5.5.35-0ubuntu0.12.10.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -27,7 +27,7 @@ CREATE TABLE `categories` (
   `user_id` int(11) NOT NULL,
   `category_name` varchar(30) NOT NULL,
   PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=91 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=92 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,32 +36,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (85,777,'без категории'),(86,777,'без категории'),(87,2,'доход11'),(88,2,'доход233'),(90,2,'ZZZZ');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `category_type`
---
-
-DROP TABLE IF EXISTS `category_type`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `category_type` (
-  `category_type_id` int(11) NOT NULL AUTO_INCREMENT,
-  `category_type_name` varchar(30) NOT NULL,
-  PRIMARY KEY (`category_type_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `category_type`
---
-
-LOCK TABLES `category_type` WRITE;
-/*!40000 ALTER TABLE `category_type` DISABLE KEYS */;
-INSERT INTO `category_type` VALUES (1,'расход'),(2,'доход');
-/*!40000 ALTER TABLE `category_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -83,7 +58,7 @@ CREATE TABLE `operations` (
   KEY `user_id` (`user_id`),
   CONSTRAINT `operations_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `operations_ibfk_4` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,7 +67,6 @@ CREATE TABLE `operations` (
 
 LOCK TABLES `operations` WRITE;
 /*!40000 ALTER TABLE `operations` DISABLE KEYS */;
-INSERT INTO `operations` VALUES (1,1,NULL,100,'2014-02-22','ss'),(2,2,87,111,'2014-02-22',''),(3,2,87,222,'2014-02-22',''),(4,2,87,-110,'2014-02-22',''),(5,2,88,2222,'2014-02-22',''),(6,2,90,-9,'2014-02-22','adasd'),(7,2,90,10,'2014-02-22','zd');
 /*!40000 ALTER TABLE `operations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,7 +84,7 @@ CREATE TABLE `users` (
   `login` varchar(30) NOT NULL,
   `password` varchar(32) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -119,7 +93,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Root','root@localhost','root','913f8cfc77c2c974cc3a838cde8c7e6b'),(2,'333','444@ddd.ccc','111','be8fe4c12c4e43217c06098a2595a950');
+INSERT INTO `users` VALUES (1,'Root','root@localhost','root','913f8cfc77c2c974cc3a838cde8c7e6b');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -132,4 +106,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-02-22 13:48:37
+-- Dump completed on 2014-02-25  0:46:07
