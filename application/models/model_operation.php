b6a61689f7b09beb95ae4cb37d0f93302c07ef71
memb37 <memb37@gmail.com>
2014-02-25 01:10:28 +0400

    у операций без категороии ставить категорию NULL

--- application/models/model_operation.php
+++ application/models/model_operation.php
@@ -32,6 +32,7 @@ class Model_Operation extends Model
 		if($_POST['optype'] == 1) {
 			$_POST['summ'] = -1 * $_POST['summ'];
 		}
+        $_POST['cat_id'] = isset($_POST['cat_id']) ? $_POST['cat_id'] :NULL;
 		try
 		{
 			$stmt = $db->prepare("INSERT INTO operations
