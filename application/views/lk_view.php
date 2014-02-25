62e82d8a8377ae80103ef20d4454f4a2e38a2d29
memb37 <memb37@gmail.com>
2014-02-25 20:33:37 +0400

    убрать category_type

--- application/views/lk_view.php
+++ application/views/lk_view.php
@@ -2,12 +2,12 @@
 	<input type = "submit" name = "outgo" value = "Добавить расход"> 
 	<input type = "submit" name = "income" value = "Добавить доход"> 
 </form>
-<? 
+<?
 if ($data)
 {
 	foreach ($data as $d)
 	{
-		echo $d['date']." ".$d['category_type_name']."  ".$d['category_name']."  ".$d['summ']."<br>";
+		echo $d['date']." ".$d['category_name']."  ".$d['summ']."<br>";
 	}
 }
 
