ad7f0a3d33227b4093dfec8a9809686782e48a70
notapresent <notapresent@gmail.com>
2014-03-02 20:43:30 +0400

    Распаковка переменных в View

--- application/core/view.php
+++ application/core/view.php
@@ -3,7 +3,8 @@
 class View {
     public $template = 'template_view.php';
 
-    function generate($content_view, $data = null) {
+    function generate($content_view, $data=array()) {
+        extract($data);
         include 'application/views/'.$this->template;
     }
 }
