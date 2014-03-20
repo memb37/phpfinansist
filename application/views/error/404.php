5c9250f7d645c07a02f7afeaac067322a0ce3fd5
notapresent <notapresent@gmail.com>
2014-03-20 23:49:16 +0400

    Приводим сообщения об ошибках к общему виду

--- application/views/error/404.php
+++ application/views/error/404.php
@@ -1,4 +1,3 @@
 <?header('HTTP/1.1 404 Not Found', true, 404);?>
-<h2>404<h2>
-<h2>Нет такой страницы</h2>
+<h2>Что-то потерялось :(</h2>
 <a href=<?=BASE_URL?>><img src="images/phpfinansist_logo_plain.svg"></a>
