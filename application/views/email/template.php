6b10db63682237ac4a143741af2d53e74c6faab2
memb37 <memb37@gmail.com>
2014-04-06 14:24:15 +0400

    переделал шаблон для писем

--- application/views/email/template.php
+++ application/views/email/template.php
@@ -3,43 +3,29 @@
 <head>
     <base href="<?=BASE_URL?>">
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <link rel="stylesheet" href="css/bootstrap.min.css">  
-       <script src="js/jquery-2.0.3.min.js"></script>
-       <script src="js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="css/style.css">
-    <title>Главная</title>
 </head>
 <body>
 
-<div class="navbar navbar-fixed-top mynavbar" role="navigation">
-    <div class="container mynavbar-item">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="<?=BASE_URL?>">
-                <img src="images/phpfinansist_logo_plain.svg">
-                phpfinansist</a>
-        </div>
-    </div>
-</div>
+<table width="50%" align="center" border="0">
+    <tr>
+        <th bgcolor="#428bca" style="font-size: 50px">
+            <a style="color: #cdddeb; text-decoration: none" href="<?=BASE_URL?>"><img src="images/phpfinansist_logo_plain.svg">
+            phpfinansist
+            </a>
+        </th>
+    </tr>
+    <tr bgcolor="#f3f3f3">
+        <td  height="300px">
+            <?php include BASE_PATH . 'application/views/'.$content_view; ?>
+        </td>
+    </tr>
+    <tr align="center" bgcolor="#bbbbbb">
+        <td>
+            подвал
+        </td>
+    </tr>
 
+</table>
 
-<div class="container"> 			<!--  HEADER-->
-    <div class="row">
-        <div class="col-md-12 text-center page-header">
-            <h1></h1>
-        </div>
-    </div>
-</div>
-
-
-<div class="col-md-6 col-md-offset-3 text-center content">		<!-- CONTENT-->
-    <?php include BASE_PATH . 'application/views/'.$content_view; ?>
-</div>
-
-<div id="footer">				<!-- FOOTER-->
-    <div class="container">
-        <p class="text-muted">Подвальчик</p>
-    </div>
-</div>
 </body>
 </html>
